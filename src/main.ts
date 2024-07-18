64aac0e71d0194e87b33a49075a42b3697f2aa68
M. Dzaki Imaduddin <dzakiimad@gmail.com>
2024-07-18 10:30:36 +0700

    tes

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors()
+  app.enableCors({
+     origin: 'localhost', // Add your frontend URL here 
+     methods: [ "GET", "POST", "PUT", "DELETE","OPTIONS", "PATCH"],
+     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  });
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
